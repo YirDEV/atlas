7d8935edc81dd5e234d7d21bc43512e005775128
YirDev <apjair97@gmail.com>
2020-07-14 03:45:46 -0500

    [Update] references view was created and conect correctly within the Django app

--- code_atlas/urls.py
+++ code_atlas/urls.py
@@ -1,21 +1,12 @@
-"""code_atlas URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# import your folders to find your app files
 from django.contrib import admin
 from django.urls import path
+from references import views as ref_views
 
+# Create your App URLS here
+# Here is where you call your view and its respective views.py functions
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('references/',ref_views.references_db),
 ]
