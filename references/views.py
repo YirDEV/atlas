4a04bf57377e4e8282a4093978f29bd2d30fceb9
YirDev <apjair97@gmail.com>
2020-07-14 04:19:46 -0500

    [CODE] added table template to app-references html

--- references/views.py
+++ references/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+refcodes = [
+{
+    'id': '1',
+    'code': '123',
+    'system': '4141',
+    'operation_number': '6021',
+    'operation_name': 'CTLMAYORTEST',
+    'hm_description': 'something wrong with the cables',
+    'fix_action' : 'Jump around the system five times and scream "Im an Idiot"'
+}
+]
 # Create your views here.
 def references_db(request):
-    return render(request, 'app-refcodes.html')
+    return render(request, 'app-refcodes.html', {'refcodes' : refcodes})
